5f8192c354ce171d109b3a76428f146265329d3a
岡田元輝 <genki0301ab@gmail.com>
2021-01-29 12:04:42 +0900

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -9,49 +9,64 @@ var connect = require("gulp-connect-php");
 // DEFAULT TASK
 //
 //------------------------------------------------------------
-gulp.task("browser-sync" , function() { 
+const task_browser_sync = function() { 
 	browserSync.init({
        proxy: "localhost"
     });
-});
+};
 
-gulp.task("reload" , function() {
+const task_reload = function() {
 	browserSync.reload();
-});
+};
 
-gulp.task("html", function() {
-	gulp.src(["./htdocs/punk/**/*html"])
+const task_html = function() {
+	gulp.src(["./htdocs/**/*html"])
 		.pipe(plumber())
 		.pipe(browserSync.reload({stream:true}));
-});
+};
 
-gulp.task("sass" , function() {
-	gulp.src(["./htdocs/punk/**/*scss"])
+const task_sass = function() {
+	gulp.src(["./htdocs/**/*scss"])
 		.pipe(plumber())
 		.pipe(sass({
 			outputStyle: "expanded"
 		}))
-		.pipe(gulp.dest("./htdocs/punk/"))
+		.pipe(gulp.dest("./htdocs/"))
 		.pipe(browserSync.reload({stream:true}));
-});
+};
 
-gulp.task("php" , function() {
-	gulp.src(["./htdocs/punk/**/*php"])
+const task_php = function() {
+	gulp.src(["./htdocs/**/*php"])
 		.pipe(plumber())
 		.pipe(browserSync.reload({stream:true}));
-});
+};
 
-gulp.task("javascript" , function() {
-	gulp.src(["./htdocs/punk/**/*js"])
+const task_javascript = function() {
+	gulp.src(["./htdocs/**/*js"])
 		.pipe(plumber())
 		.pipe(browserSync.reload({stream:true}));
-});
+};
 
-gulp.task("watch" , function() {
-	gulp.watch(["./htdocs/punk/**/*html"] , ["html"]);
-	gulp.watch(["./htdocs/punk/**/*php"] , ["php"]);
-	gulp.watch(["./htdocs/punk/**/*scss"] , ["sass"]);
-	gulp.watch(["./htdocs/punk/**/*js"] , ["javascript"]);
-});
+const task_watch = function() {
+	//html
+	gulp.watch("./htdocs/**/*html").on("change", task_html);
+	gulp.watch("./htdocs/**/*html").on("add", task_html);
+	gulp.watch("./htdocs/**/*html").on("unlink", task_html);
 
-gulp.task("default" , ["browser-sync" , "watch"]); 
+	//php
+	gulp.watch("./htdocs/**/*php").on("change", task_php);
+	gulp.watch("./htdocs/**/*php").on("add", task_php);
+	gulp.watch("./htdocs/**/*php").on("unlink", task_php);
+
+	//sass
+	gulp.watch("./htdocs/**/*scss").on("change", task_sass);
+	gulp.watch("./htdocs/**/*scss").on("add", task_sass);
+	gulp.watch("./htdocs/**/*scss").on("unlink", task_sass);
+
+	//javascript
+	gulp.watch("./htdocs/**/*js").on("change", task_javascript);
+	gulp.watch("./htdocs/**/*js").on("add", task_javascript);
+	gulp.watch("./htdocs/**/*js").on("unlink", task_javascript);
+};
+
+gulp.task("default", gulp.parallel(task_browser_sync, task_watch));
